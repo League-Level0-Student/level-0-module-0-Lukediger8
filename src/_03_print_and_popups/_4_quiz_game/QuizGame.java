85dd9ecfced8cccce706ea92b08fd0b962624f50
student <student@ip-172-31-48-250.us-west-2.compute.internal>
2021-10-02 12:51:03 -0700

    pixil-frame

--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -16,15 +16,42 @@ public class QuizGame {
 			JOptionPane.showMessageDialog(null, "correct");
 			// 4. if the user's answer was correct, add one to their score
 		} else {
+			JOptionPane.showMessageDialog(null, "incorrect");
 			score -= 1;
 		}
 		// MAKE MORE QUESTIONS. Ask more questions by repeating the above
 		// Option: Subtract a point from their score for a wrong answer
 		input = JOptionPane.showInputDialog("what is 2+49");
 		if (input.equals("51")) {
-		 score+=1;
+			score += 1;
+			JOptionPane.showMessageDialog(null, "correct");
 		}
-		// After all the questions have been asked, tell the user their final score
 
+		// After all the questions have been asked, tell the user their final score
+		else {
+			score -= 1;
+			JOptionPane.showMessageDialog(null, "incorrect");
+		}
+		input = JOptionPane.showInputDialog("which type of talkis are the best");
+		if (input.equals("fuego")) {
+			score += 1;
+			JOptionPane.showMessageDialog(null, "correct");
+		} else {
+			JOptionPane.showMessageDialog(null, "incorrect");
+
+			score -= 1;
+
+		}
+		input = JOptionPane.showInputDialog("who is the richest person on earth");
+		if (input.equals("Jeff Bezos")) {
+			score += 1;
+			JOptionPane.showMessageDialog(null, "corect");
+
+		}
+		else {
+			JOptionPane.showMessageDialog(null,"incorrect");
+			score -= 1;
+		}
+		JOptionPane.showMessageDialog(null,"your total score is "+score);
 	}
 }
--- src/_03_print_and_popups/_5_awesome_or_not/AwesomeOrNot.java
+++ src/_03_print_and_popups/_5_awesome_or_not/AwesomeOrNot.java
@@ -2,35 +2,50 @@ package _03_print_and_popups._5_awesome_or_not;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class AwesomeOrNot {
 
 	// 1. Make a main method that includes everything below
 
+	public static void main(String[] args) {
+		
 	
 		Random ran = new Random();    //This will be used below to make a random number. 
 		
 		// 2. Make a variable that will hold a random whole number
-	
+		int number = ran.nextInt(4);
 		// 3. Set your variable equal to a positive number less than 4 using     ran.nextInt(4); 
-	
+		System.out.println(number);
 		// 3. Print your variable to the console
-	
+		String input = JOptionPane.showInputDialog("enter soemthing cool");
 		// 4. Get the user to enter something that they think is awesome
-	
+		
 		// 5. If your variable is  0
-	
-			// -- tell the user whatever they entered is awesome!
-	
+		
+					// -- tell the user whatever they entered is awesome!
+				
+		if (number == 0) {
+			JOptionPane.showMessageDialog(null,"that's awsome!");
+		}
+		
+		
 		// 6. If your variable is  1
-	
+		if (number == 1) {
+			JOptionPane.showMessageDialog(null,"thats ok");
+		}
 			// -- tell the user whatever they entered is ok.
-	
+			
 		// 7. If your variable is  2
-	
+		if (number == 2) {
+			JOptionPane.showMessageDialog(null,"thats boring");
+		}
 			// -- tell the user whatever they entered is boring.
 	
 		// 8. If your variable is  3
-	
+		if (number == 3) {
+			JOptionPane.showMessageDialog(null, "you are pro");
+		}
 			// -- invent your own message to give to the user (be nice).
-
+	}
 }
